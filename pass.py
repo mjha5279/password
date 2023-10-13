bacb47d01e04232b5081e66b4c71493ea4068c87
mjha5279 <143924150+mjha5279@users.noreply.github.com>
2023-10-13 21:25:17 +0530

    passw.py

--- pass.py
+++ pass.py
@@ -1,12 +1,21 @@
-class base:
-    def __init__(self):
-        print("hii")
-class derived(base):
-    def __init__(self):
-        print("hello")
-
-o=derived()
-
+import random
+import string
+s=string.ascii_lowercase
+s1=string.ascii_uppercase
+s2=string.digits
+s3=string.punctuation
+s4=[]
+s4.extend(s)
+s4.extend(s1)
+s4.extend(s2)
+s4.extend(s3)
+try:
+    print("Enter length of password--")
+    l=int(input())
+    print("your password is--")
+    print("".join(random.sample(s4,l)))
+except Exception as Valueerror:
+    print(" Warning:---  Enter integer")
 
 
 
